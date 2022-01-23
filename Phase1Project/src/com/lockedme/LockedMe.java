35805f0f496b36646a2ea09c6f97b18c08eaf6f4
Siva Reddy <dornalasivareddy@gmail.com>
2022-01-23 08:40:03 +0530

    Phase1 Project structure layout

---
+++ Phase1Project/src/com/lockedme/LockedMe.java
@@ -0,0 +1,39 @@
+package com.lockedme;
+
+public class LockedMe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	/**
+	 * This method will display all the files present in the directory
+	 */
+	public void diaplayAllFiles()
+	{
+		
+	}
+	/**
+	 * This method will create the files
+	 */
+	public void createFiles()
+	{
+		
+	}
+	
+	/**
+	 * This method will search for the files present in the directory
+	 */
+	public void searchFiles()
+	{
+		
+	}
+	/**
+	 * This method will delete the files
+	 */
+	public void deleteFiles()
+	{
+		
+	}
+
+}
